7d9257c0c9827a6c31c629a2e142e00910f6cf2a
User <User@LAB3-3>
2024-01-30 15:02:47 +0200

    Update Lesson 30/01/2024

--- ViewModels/FindMonkeyByLocationPageViewModel.cs
+++ ViewModels/FindMonkeyByLocationPageViewModel.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿using MonkeysMVVM.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace MonkeysMVVM.ViewModels
 {
     public class FindMonkeyByLocationPageViewModel:ViewModel
     {
+        private string country;
+        private int count;
+        public string Country {  get { return country; } set {  country = value; OnPropertyChanged();((Command)SearchByCountryCommand).ChangeCanExecute(); } }
+        public int Count { get { return count; } set { count = value; OnPropertyChanged(); } }  
+        public ICommand SearchByCountryCommand { get; set; }
+        private Monkey monkey;
+        public string Name { get { return monkey.Name; } }
+        public string ImageUrl { get { return monkey.ImageUrl; } }
+
+        public FindMonkeyByLocationPageViewModel()
+        {
+            monkey = new Monkey() { Name = "אין קופים כרגע" };
+            SearchByCountryCommand = new Command(FindMonkeys, () => String.IsNullOrEmpty(Country));
+          
+        }
+
+        private void FindMonkeys()
+        {
+            
+        }
     }
+
 }
